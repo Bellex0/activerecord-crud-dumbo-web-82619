def9308770a31d148204ff710c97463fce49f97f
Belle P <bellepx3@gmail.com>
2019-09-07 21:26:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -82,7 +82,8 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   
-  __
+  movie = Movie.find_by(name: 'Wat?')
+  movie.update(name: 'Wat, huh?')
 end
 
 def can_update_multiple_items_at_once
