3cf7e5eed505e4c58da3d852f0a785aafc25d7df
Belle P <bellepx3@gmail.com>
2019-09-07 20:49:35 +0000

    Automatically backed up by Learn

--- db/migrate/20190907204527_create_movies.rb
+++ db/migrate/20190907204527_create_movies.rb
@@ -1,10 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :name
-      t.float :price
-      t.string :image_url
-      t.string :size
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
       t.timestamps
     end
     
